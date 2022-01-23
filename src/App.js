daa40465edbe19bad2992332fb7d16fecd9676a2
Wenhan Yang <574499742@qq.com>
2022-01-23 12:39:38 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Button, TreeSelect} from 'antd';
+
+const {TreeNode} = TreeSelect;
+
+let loaded = false;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState(undefined);
+    const onChange = (newValue) => {
+        alert(newValue);
+    };
+
+    const [categoryRoot, setCategoryRoot] = useState("");
+
+    const buildTree = (category) => {
+        if (!category) {
+            return;
+        }
+        if (!category.children || category.children.length === 0) {
+            return <TreeNode value={category.categoryId} title={category.name} key={category.categoryId}></TreeNode>
+        }
+        return <TreeNode value={category.categoryId} title={category.name} key={category.categoryId}>
+            {category.children.map(childCategory => {
+                return buildTree(childCategory);
+            })}
+        </TreeNode>
+    }
+
+    const handlReduxHookBtnClick = () => {
+         fetch('http://localhost:8080/')
+            .then(response => {
+                loaded = true;
+                return response.json();
+            })
+            .then(categoryRoot => {
+                if (categoryRoot == null) {
+                    return;
+                }
+                setCategoryRoot(categoryRoot);
+                console.log("refresh tree");
+            });
+    }
+
+    return (
+        <>
+            <Button type="primary" onClick={handlReduxHookBtnClick}>Redux Hook</Button>
+            <TreeSelect
+                showSearch
+                style={{width: '100%'}}
+                dropdownStyle={{maxHeight: 400, overflow: 'auto'}}
+                placeholder="Please select"
+                allowClear
+                treeDefaultExpandAll
+                onChange={onChange}
+            >
+                {buildTree(categoryRoot)}
+            </TreeSelect>
+        </>
+    );
 }
 
 export default App;
